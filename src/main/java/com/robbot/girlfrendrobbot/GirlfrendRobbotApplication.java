d9f835bda6c4870b2b37408696b0c59035f369ad
q827505931 <a827505931@qq.com>
2020-12-08 14:11:13 +0800

    first commit

---
+++ src/main/java/com/robbot/girlfrendrobbot/GirlfrendRobbotApplication.java
@@ -0,0 +1,13 @@
+package com.robbot.girlfrendrobbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlfrendRobbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GirlfrendRobbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robbot/girlfrendrobbot/GirlfrendRobbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robbot.girlfrendrobbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlfrendRobbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
